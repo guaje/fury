a9f66c1a1caa5d5bf5d0f369ebc8f59adea61d41
Javier Guaje <jrguajeg@iu.edu>
2023-02-26 11:28:05 -0500

    Added cortical surface actor and Blinn-Phong shading to Schlick's demo.

--- docs/examples/viz_quals_roughness.py
+++ docs/examples/viz_quals_roughness.py
@@ -85,124 +85,130 @@ def win_callback(obj, event):
         control_panel.re_align(size_change)
 
 
-scene = window.Scene()
-scene.background((1, 1, 1))
-
-sphere = actor.sphere([[0, 0, 0]], (0, 0, 0), radii=2, theta=64, phi=64)
-
-doa = [0, 1, 0.5]
-
-normals = normals_from_actor(sphere)
-tangents = tangents_from_direction_of_anisotropy(normals, doa)
-tangents_to_actor(sphere, tangents)
-
-pbr_params = material.manifest_pbr(sphere, roughness=.5)
-
-scene.add(sphere)
-
-show_m = window.ShowManager(
-    scene=scene, size=(1920, 1080), reset_camera=False, order_transparent=True)
-
-control_panel = ui.Panel2D(
-    (400, 500), position=(5, 5), color=(0.25, 0.25, 0.25), opacity=0.75,
-    align='right')
-
-slider_label_metallic = ui.TextBlock2D(text='Metallic', font_size=16)
-slider_label_roughness = ui.TextBlock2D(text='Roughness', font_size=16)
-slider_label_anisotropy = ui.TextBlock2D(text='Anisotropy', font_size=16)
-slider_label_anisotropy_rotation = ui.TextBlock2D(
-    text='Anisotropy Rotation', font_size=16)
-slider_label_anisotropy_direction_x = ui.TextBlock2D(
-    text='Anisotropy Direction X', font_size=16)
-slider_label_anisotropy_direction_y = ui.TextBlock2D(
-    text='Anisotropy Direction Y', font_size=16)
-slider_label_anisotropy_direction_z = ui.TextBlock2D(
-    text='Anisotropy Direction Z', font_size=16)
-slider_label_coat_strength = ui.TextBlock2D(
-    text='Coat Strength', font_size=16)
-slider_label_coat_roughness = ui.TextBlock2D(
-    text='Coat Roughness', font_size=16)
-slider_label_base_ior = ui.TextBlock2D(text='Base IoR', font_size=16)
-slider_label_coat_ior = ui.TextBlock2D(text='Coat IoR', font_size=16)
-
-control_panel.add_element(slider_label_metallic, (0.01, 0.95))
-control_panel.add_element(slider_label_roughness, (0.01, 0.86))
-control_panel.add_element(slider_label_anisotropy, (0.01, 0.77))
-control_panel.add_element(slider_label_anisotropy_rotation, (0.01, 0.68))
-control_panel.add_element(slider_label_anisotropy_direction_x, (0.01, 0.59))
-control_panel.add_element(slider_label_anisotropy_direction_y, (0.01, 0.5))
-control_panel.add_element(slider_label_anisotropy_direction_z, (0.01, 0.41))
-control_panel.add_element(slider_label_coat_strength, (0.01, 0.32))
-control_panel.add_element(slider_label_coat_roughness, (0.01, 0.23))
-control_panel.add_element(slider_label_base_ior, (0.01, 0.14))
-control_panel.add_element(slider_label_coat_ior, (0.01, 0.05))
-
-slider_slice_metallic = ui.LineSlider2D(
-    initial_value=pbr_params.metallic, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_roughness = ui.LineSlider2D(
-    initial_value=pbr_params.roughness, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_anisotropy = ui.LineSlider2D(
-    initial_value=pbr_params.anisotropy, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_anisotropy_rotation = ui.LineSlider2D(
-    initial_value=pbr_params.anisotropy_rotation, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_coat_strength = ui.LineSlider2D(
-    initial_value=pbr_params.coat_strength, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_coat_roughness = ui.LineSlider2D(
-    initial_value=pbr_params.coat_roughness, max_value=1, length=195,
-    text_template='{value:.1f}')
-
-slider_slice_anisotropy_direction_x = ui.LineSlider2D(
-    initial_value=doa[0], min_value=-1, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_anisotropy_direction_y = ui.LineSlider2D(
-    initial_value=doa[1], min_value=-1, max_value=1, length=195,
-    text_template='{value:.1f}')
-slider_slice_anisotropy_direction_z = ui.LineSlider2D(
-    initial_value=doa[2], min_value=-1, max_value=1, length=195,
-    text_template='{value:.1f}')
-
-slider_slice_base_ior = ui.LineSlider2D(
-    initial_value=pbr_params.base_ior, min_value=1, max_value=2.3, length=195,
-    text_template='{value:.02f}')
-slider_slice_coat_ior = ui.LineSlider2D(
-    initial_value=pbr_params.coat_ior, min_value=1, max_value=2.3, length=195,
-    text_template='{value:.02f}')
-
-slider_slice_metallic.on_change = change_slice_metallic
-slider_slice_roughness.on_change = change_slice_roughness
-slider_slice_anisotropy.on_change = change_slice_anisotropy
-slider_slice_anisotropy_rotation.on_change = change_slice_anisotropy_rotation
-slider_slice_anisotropy_direction_x.on_change = change_slice_anisotropy_direction_x
-slider_slice_anisotropy_direction_y.on_change = change_slice_anisotropy_direction_y
-slider_slice_anisotropy_direction_z.on_change = change_slice_anisotropy_direction_z
-slider_slice_coat_strength.on_change = change_slice_coat_strength
-slider_slice_coat_roughness.on_change = change_slice_coat_roughness
-slider_slice_base_ior.on_change = change_slice_base_ior
-slider_slice_coat_ior.on_change = change_slice_coat_ior
-
-control_panel.add_element(slider_slice_metallic, (0.44, 0.95))
-control_panel.add_element(slider_slice_roughness, (0.44, 0.86))
-control_panel.add_element(slider_slice_anisotropy, (0.44, 0.77))
-control_panel.add_element(slider_slice_anisotropy_rotation, (0.44, 0.68))
-control_panel.add_element(slider_slice_anisotropy_direction_x, (0.44, 0.59))
-control_panel.add_element(slider_slice_anisotropy_direction_y, (0.44, 0.5))
-control_panel.add_element(slider_slice_anisotropy_direction_z, (0.44, 0.41))
-control_panel.add_element(slider_slice_coat_strength, (0.44, 0.32))
-control_panel.add_element(slider_slice_coat_roughness, (0.44, 0.23))
-control_panel.add_element(slider_slice_base_ior, (0.44, 0.14))
-control_panel.add_element(slider_slice_coat_ior, (0.44, 0.05))
-
-scene.add(control_panel)
-
-show_m.iren.AddObserver('KeyPressEvent', key_pressed)
-
-size = scene.GetSize()
-
-show_m.add_window_callback(win_callback)
-
-show_m.start()
+if __name__ == '__main__':
+    scene = window.Scene()
+    scene.background((1, 1, 1))
+    
+    sphere = actor.sphere([[0, 0, 0]], (0, 0, 0), radii=2, theta=64, phi=64)
+    
+    doa = [0, 1, 0.5]
+    
+    normals = normals_from_actor(sphere)
+    tangents = tangents_from_direction_of_anisotropy(normals, doa)
+    tangents_to_actor(sphere, tangents)
+    
+    pbr_params = material.manifest_pbr(sphere, roughness=.5)
+    
+    scene.add(sphere)
+    
+    show_m = window.ShowManager(
+        scene=scene, size=(1920, 1080), reset_camera=False,
+        order_transparent=True)
+    
+    control_panel = ui.Panel2D(
+        (400, 500), position=(5, 5), color=(0.25, 0.25, 0.25), opacity=0.75,
+        align='right')
+    
+    slider_label_metallic = ui.TextBlock2D(text='Metallic', font_size=16)
+    slider_label_roughness = ui.TextBlock2D(text='Roughness', font_size=16)
+    slider_label_anisotropy = ui.TextBlock2D(text='Anisotropy', font_size=16)
+    slider_label_anisotropy_rotation = ui.TextBlock2D(
+        text='Anisotropy Rotation', font_size=16)
+    slider_label_anisotropy_direction_x = ui.TextBlock2D(
+        text='Anisotropy Direction X', font_size=16)
+    slider_label_anisotropy_direction_y = ui.TextBlock2D(
+        text='Anisotropy Direction Y', font_size=16)
+    slider_label_anisotropy_direction_z = ui.TextBlock2D(
+        text='Anisotropy Direction Z', font_size=16)
+    slider_label_coat_strength = ui.TextBlock2D(
+        text='Coat Strength', font_size=16)
+    slider_label_coat_roughness = ui.TextBlock2D(
+        text='Coat Roughness', font_size=16)
+    slider_label_base_ior = ui.TextBlock2D(text='Base IoR', font_size=16)
+    slider_label_coat_ior = ui.TextBlock2D(text='Coat IoR', font_size=16)
+    
+    control_panel.add_element(slider_label_metallic, (0.01, 0.95))
+    control_panel.add_element(slider_label_roughness, (0.01, 0.86))
+    control_panel.add_element(slider_label_anisotropy, (0.01, 0.77))
+    control_panel.add_element(slider_label_anisotropy_rotation, (0.01, 0.68))
+    control_panel.add_element(
+        slider_label_anisotropy_direction_x, (0.01, 0.59))
+    control_panel.add_element(slider_label_anisotropy_direction_y, (0.01, 0.5))
+    control_panel.add_element(
+        slider_label_anisotropy_direction_z, (0.01, 0.41))
+    control_panel.add_element(slider_label_coat_strength, (0.01, 0.32))
+    control_panel.add_element(slider_label_coat_roughness, (0.01, 0.23))
+    control_panel.add_element(slider_label_base_ior, (0.01, 0.14))
+    control_panel.add_element(slider_label_coat_ior, (0.01, 0.05))
+    
+    slider_slice_metallic = ui.LineSlider2D(
+        initial_value=pbr_params.metallic, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_roughness = ui.LineSlider2D(
+        initial_value=pbr_params.roughness, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_anisotropy = ui.LineSlider2D(
+        initial_value=pbr_params.anisotropy, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_anisotropy_rotation = ui.LineSlider2D(
+        initial_value=pbr_params.anisotropy_rotation, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_coat_strength = ui.LineSlider2D(
+        initial_value=pbr_params.coat_strength, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_coat_roughness = ui.LineSlider2D(
+        initial_value=pbr_params.coat_roughness, max_value=1, length=195,
+        text_template='{value:.1f}')
+    
+    slider_slice_anisotropy_direction_x = ui.LineSlider2D(
+        initial_value=doa[0], min_value=-1, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_anisotropy_direction_y = ui.LineSlider2D(
+        initial_value=doa[1], min_value=-1, max_value=1, length=195,
+        text_template='{value:.1f}')
+    slider_slice_anisotropy_direction_z = ui.LineSlider2D(
+        initial_value=doa[2], min_value=-1, max_value=1, length=195,
+        text_template='{value:.1f}')
+    
+    slider_slice_base_ior = ui.LineSlider2D(
+        initial_value=pbr_params.base_ior, min_value=1, max_value=2.3,
+        length=195, text_template='{value:.02f}')
+    slider_slice_coat_ior = ui.LineSlider2D(
+        initial_value=pbr_params.coat_ior, min_value=1, max_value=2.3,
+        length=195, text_template='{value:.02f}')
+    
+    slider_slice_metallic.on_change = change_slice_metallic
+    slider_slice_roughness.on_change = change_slice_roughness
+    slider_slice_anisotropy.on_change = change_slice_anisotropy
+    slider_slice_anisotropy_rotation.on_change = change_slice_anisotropy_rotation
+    slider_slice_anisotropy_direction_x.on_change = change_slice_anisotropy_direction_x
+    slider_slice_anisotropy_direction_y.on_change = change_slice_anisotropy_direction_y
+    slider_slice_anisotropy_direction_z.on_change = change_slice_anisotropy_direction_z
+    slider_slice_coat_strength.on_change = change_slice_coat_strength
+    slider_slice_coat_roughness.on_change = change_slice_coat_roughness
+    slider_slice_base_ior.on_change = change_slice_base_ior
+    slider_slice_coat_ior.on_change = change_slice_coat_ior
+    
+    control_panel.add_element(slider_slice_metallic, (0.44, 0.95))
+    control_panel.add_element(slider_slice_roughness, (0.44, 0.86))
+    control_panel.add_element(slider_slice_anisotropy, (0.44, 0.77))
+    control_panel.add_element(slider_slice_anisotropy_rotation, (0.44, 0.68))
+    control_panel.add_element(
+        slider_slice_anisotropy_direction_x, (0.44, 0.59))
+    control_panel.add_element(slider_slice_anisotropy_direction_y, (0.44, 0.5))
+    control_panel.add_element(
+        slider_slice_anisotropy_direction_z, (0.44, 0.41))
+    control_panel.add_element(slider_slice_coat_strength, (0.44, 0.32))
+    control_panel.add_element(slider_slice_coat_roughness, (0.44, 0.23))
+    control_panel.add_element(slider_slice_base_ior, (0.44, 0.14))
+    control_panel.add_element(slider_slice_coat_ior, (0.44, 0.05))
+    
+    scene.add(control_panel)
+    
+    show_m.iren.AddObserver('KeyPressEvent', key_pressed)
+    
+    size = scene.GetSize()
+    
+    show_m.add_window_callback(win_callback)
+    
+    show_m.start()
--- docs/examples/viz_quals_schlick.py
+++ docs/examples/viz_quals_schlick.py
@@ -1,7 +1,29 @@
 import os
 
-from fury import actor, material, ui, window
+from nilearn import datasets, surface
+
+from fury import actor, window
+from fury.lib import PolyData
 from fury.shaders import compose_shader, import_fury_shader, shader_to_actor
+from fury.utils import (
+    get_actor_from_polydata,
+    rotate,
+    set_polydata_colors,
+    set_polydata_triangles,
+    set_polydata_vertices,
+)
+
+
+def get_hemisphere_actor():
+    fsaverage = datasets.fetch_surf_fsaverage(mesh='fsaverage')
+    points, triangles = surface.load_surf_mesh(fsaverage.infl_right)
+    polydata = PolyData()
+    set_polydata_vertices(polydata, points)
+    set_polydata_triangles(polydata, triangles)
+    hemi_actor = get_actor_from_polydata(polydata)
+    rotate(hemi_actor, rotation=(-90, 0, 0, 1))
+    rotate(hemi_actor, rotation=(-80, 1, 0, 0))
+    return hemi_actor
 
 
 def key_pressed(obj, event):
@@ -21,53 +43,69 @@ def win_callback(obj, event):
         size_change = [size[0] - size_old[0], 0]
 
 
-scene = window.Scene()
-scene.background((1, 1, 1))
-
-sphere = actor.sphere([[0, 0, 0]], (0, 0, 0), radii=2, theta=64, phi=64)
-
-pow5 = import_fury_shader(os.path.join('utils', 'pow5.glsl'))
-
-schlick_weight = import_fury_shader(
-    os.path.join('lighting', 'schlick_weight.frag')
-)
-
-fs_decl = compose_shader([pow5, schlick_weight])
-
-shader_to_actor(sphere, 'fragment', decl_code=fs_decl)
-
-normal = 'vec3 normal = normalVCVSOutput;'
-view = 'vec3 view = normalize(-vertexVC.xyz);'
-dot_n_v = 'float dotNV = clamp(dot(normal, view), 1e-5, 1);'
-fsw = 'float fsw = schlickWeight(dotNV);'
-frag_output = \
-    """
-    if(fsw <= .0022)
-    {
-        fragOutput0 = vec4(vec3(1, 0, 0), opacity);
-    }
-    else
-    {
-        if(fsw <= .22)
-            fragOutput0 = vec4(vec3(1, 1, 0), opacity);
+if __name__ == '__main__':
+    scene = window.Scene()
+    scene.background((1, 1, 1))
+    
+    #obj_actor = actor.sphere([[0, 0, 0]], (0, 0, 0), radii=2, theta=64, phi=64)
+    obj_actor = get_hemisphere_actor()
+    
+    pow5 = import_fury_shader(os.path.join('utils', 'pow5.glsl'))
+    
+    schlick_weight = import_fury_shader(
+        os.path.join('lighting', 'schlick_weight.frag')
+    )
+    
+    blinn_phong_model = import_fury_shader(
+        os.path.join('lighting', 'blinn_phong_model.frag')
+    )
+    
+    fs_decl = compose_shader([pow5, schlick_weight, blinn_phong_model])
+    
+    shader_to_actor(obj_actor, 'fragment', decl_code=fs_decl)
+    
+    normal = 'vec3 normal = normalVCVSOutput;'
+    view = 'vec3 view = normalize(-vertexVC.xyz);'
+    dot_n_v = 'float dotNV = clamp(dot(normal, view), 1e-5, 1);'
+    fsw = 'float fsw = schlickWeight(dotNV);'
+    frag_output = \
+        """
+        vec3 color = vec3(0);
+        if(fsw <= .0022)
+        {
+            color = vec3(1, 0, 0);
+        }
         else
-            fragOutput0 = vec4(vec3(0, 1, 0), opacity);
-    }
-    """
-
-fs_impl = compose_shader([normal, view, dot_n_v, fsw, frag_output])
-
-shader_to_actor(sphere, 'fragment', impl_code=fs_impl, block='light')
-
-scene.add(sphere)
-
-show_m = window.ShowManager(
-    scene=scene, size=(1920, 1080), reset_camera=False, order_transparent=True)
-
-show_m.iren.AddObserver('KeyPressEvent', key_pressed)
-
-size = scene.GetSize()
-
-show_m.add_window_callback(win_callback)
-
-show_m.start()
+        {
+            if(fsw <= .22)
+            {
+                color = vec3(1, 1, 0);
+            }
+            else
+            {
+                color = vec3(0, 1, 0);
+            }
+        }
+        color = blinnPhongIllumModel(
+            dotNV, lightColor0, color, specularPower, specularColor, 
+            ambientColor);
+        fragOutput0 = vec4(color, opacity);
+        """
+    
+    fs_impl = compose_shader([normal, view, dot_n_v, fsw, frag_output])
+    
+    shader_to_actor(obj_actor, 'fragment', impl_code=fs_impl, block='light')
+    
+    scene.add(obj_actor)
+    
+    show_m = window.ShowManager(
+        scene=scene, size=(1920, 1080), reset_camera=False,
+        order_transparent=True)
+    
+    show_m.iren.AddObserver('KeyPressEvent', key_pressed)
+    
+    size = scene.GetSize()
+    
+    show_m.add_window_callback(win_callback)
+    
+    show_m.start()
