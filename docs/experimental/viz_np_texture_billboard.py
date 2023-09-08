23d6c26a05794470e7eb00bb63dfc6879316dea6
Javier Guaje <jrguajeg@iu.edu>
2023-09-08 18:34:02 -0400

    Added viz_np_texture_billboard experiment.

---
+++ docs/experimental/viz_np_texture_billboard.py
@@ -0,0 +1,46 @@
+import os
+
+import numpy as np
+
+from fury import actor, window
+from fury.lib import FloatArray, Texture
+from fury.utils import rgb_to_vtk, set_polydata_tcoords
+
+if __name__ == '__main__':
+    scene = window.Scene()
+    scene.background((1, 1, 1))
+
+    centers = np.array([[0, 0, 0]])
+
+    texture_actor = actor.billboard(centers, colors=(1, 1, 1))
+
+    actor_pd = texture_actor.GetMapper().GetInput()
+
+    # TODO: Study and understand UV texture coordinates
+    uv_vals = np.array([[0, 0], [0, 1], [1, 1], [1, 0]])
+
+    num_pnts = uv_vals.shape[0]
+
+    t_coords = FloatArray()
+    t_coords.SetNumberOfComponents(2)
+    t_coords.SetNumberOfTuples(num_pnts)
+
+    [t_coords.SetTuple(i, uv_vals[i]) for i in range(num_pnts)]
+
+    set_polydata_tcoords(actor_pd, t_coords)
+
+    np.random.seed(8)
+    #arr = np.random.randn(2, 2, 3)
+    arr = np.array([[[255, 0, 0], [255, 255, 0]], [[0, 255, 0], [0, 0, 255]]])
+    grid = rgb_to_vtk(arr.astype(np.uint8))
+
+    texture = Texture()
+    texture.SetInputDataObject(grid)
+    texture.Update()
+
+    # NOTE: Configure textures (needs TCoords on the mesh)
+    texture_actor.GetProperty().SetTexture('texture0', texture)
+
+    scene.add(texture_actor)
+
+    window.show(scene)
