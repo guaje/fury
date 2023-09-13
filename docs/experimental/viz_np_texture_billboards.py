9c9c0204b03141200c9703e4c437ad934a4b01c5
Javier Guaje <jrguajeg@iu.edu>
2023-09-13 10:40:17 -0400

    Black formatted viz_np_texture_billboards experiment.

--- docs/experimental/viz_np_texture_billboards.py
+++ docs/experimental/viz_np_texture_billboards.py
@@ -4,24 +4,31 @@ from fury import actor, window
 from fury.lib import FloatArray, Texture
 from fury.utils import rgb_to_vtk, set_polydata_tcoords
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     scene = window.Scene()
     scene.background((1, 1, 1))
 
-    centers = np.array([[0, 0, 0], [-2.5, -2, 0], [-1, -2, 0], [1, -2, 0],
-                        [2.5, -2, 0]])
-    scales = [1, .5, .5, .5, .5]
+    centers = np.array(
+        [[0, 0, 0], [-2.5, -2, 0], [-1, -2, 0], [1, -2, 0], [2.5, -2, 0]]
+    )
+    scales = [1, 0.5, 0.5, 0.5, 0.5]
 
     texture_actor = actor.billboard(centers, colors=(1, 1, 1), scales=scales)
 
     actor_pd = texture_actor.GetMapper().GetInput()
 
     # TODO: Study and understand UV texture coordinates
-    uv_vals = np.array([[0, 0], [0, 1], [1, 1], [1, 0],
-                        [0, .5], [0, 1], [.5, 1], [.5, .5],
-                        [.5, .5], [.5, 1], [1, 1], [1, .5],
-                        [0, 0], [0, .5], [.5, .5], [.5, 0],
-                        [.5, 0], [.5, .5], [1, .5], [1, 0]])
+    # fmt: off
+    uv_vals = np.array(
+        [
+            [0, 0], [0, 1], [1, 1], [1, 0],  # Full texture
+            [0, 0.5], [0, 1], [0.5, 1], [0.5, 0.5],  # Top left color
+            [0.5, 0.5], [0.5, 1], [1, 1], [1, 0.5],  # Top right color
+            [0, 0], [0, 0.5], [0.5, 0.5], [0.5, 0],  # Bottom left color
+            [0.5, 0], [0.5, 0.5], [1, 0.5], [1, 0]  # Bottom right color
+        ]
+    )
+    # fmt: on
 
     num_pnts = uv_vals.shape[0]
 
@@ -34,7 +41,7 @@ if __name__ == '__main__':
     set_polydata_tcoords(actor_pd, t_coords)
 
     np.random.seed(8)
-    #arr = np.random.randn(2, 2, 3) * 255
+    # arr = np.random.randn(2, 2, 3) * 255
     arr = np.array([[[255, 0, 0], [255, 255, 0]], [[0, 255, 0], [0, 0, 255]]])
     grid = rgb_to_vtk(arr.astype(np.uint8))
 
@@ -43,7 +50,7 @@ if __name__ == '__main__':
     texture.Update()
 
     # NOTE: Configure textures (needs TCoords on the mesh)
-    texture_actor.GetProperty().SetTexture('texture0', texture)
+    texture_actor.GetProperty().SetTexture("texture0", texture)
 
     scene.add(texture_actor)
 
