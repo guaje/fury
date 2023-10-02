f51cdb8dbab69cdd5caf63808421dab5b5076b16
Javier Guaje <jrguajeg@iu.edu>
2023-10-02 14:34:16 -0400

    Replaced rgb_to_vtk by the more flexible numpy_to_vtk_image_data function.

--- docs/experimental/viz_shader_texture.py
+++ docs/experimental/viz_shader_texture.py
@@ -12,16 +12,16 @@ from fury.lib import (
     Texture,
 )
 from fury.shaders import replace_shader_in_actor, shader_to_actor
-from fury.utils import rgb_to_vtk
+from fury.utils import numpy_to_vtk_image_data
 
 scene = window.Scene()
 
-selected_actor = 'sphere'
+selected_actor = "sphere"
 
-if selected_actor == 'cube':
+if selected_actor == "cube":
     canvas_actor = cube()
 
-if selected_actor == 'sphere':
+if selected_actor == "sphere":
     # Generate an sphere polydata
     sphere = SphereSource()
     sphere.SetThetaResolution(300)
@@ -39,10 +39,10 @@ if selected_actor == 'sphere':
 texture = Texture()
 texture.CubeMapOn()
 
-selected_texture = 'numpy'
+selected_texture = "numpy"
 
-if selected_texture == 'file':
-    file = 'sugar.jpg'
+if selected_texture == "file":
+    file = "sugar.jpg"
     imgReader = JPEGReader()
     imgReader.SetFileName(file)
     for i in range(6):
@@ -51,27 +51,25 @@ if selected_texture == 'file':
         flip.SetFilteredAxis(1)
         texture.SetInputConnection(i, flip.GetOutputPort())
 
-if selected_texture == 'numpy':
+if selected_texture == "numpy":
     arr = 255 * np.random.randn(512, 512, 3)
     arr[:256] = np.array([255, 0, 0])
-    grid = rgb_to_vtk(arr.astype(np.uint8))
+    grid = numpy_to_vtk_image_data(arr.astype(np.uint8))
     for i in range(6):
         texture.SetInputDataObject(i, grid)
 
 canvas_actor.SetTexture(texture)
 
-vs_dec = 'out vec3 TexCoords;'
-vs_impl = \
-"""
+vs_dec = "out vec3 TexCoords;"
+vs_impl = """
 vec3 camPos = -MCVCMatrix[3].xyz * mat3(MCVCMatrix);
 TexCoords = reflect(vertexMC.xyz - camPos, normalize(normalMC));
 //TexCoords = normalMC;
 //TexCoords = vertexMC.xyz;
 """
-shader_to_actor(canvas_actor, 'vertex', decl_code=vs_dec, impl_code=vs_impl)
+shader_to_actor(canvas_actor, "vertex", decl_code=vs_dec, impl_code=vs_impl)
 
-fs_code = \
-"""
+fs_code = """
 //VTK::System::Dec  // Always start with this line
 //VTK::Output::Dec  // Always have this line in your shader
 in vec3 TexCoords;
@@ -82,7 +80,7 @@ void main()
     gl_FragData[0] = texture(texture_0, TexCoords);
 }
 """
-replace_shader_in_actor(canvas_actor, 'fragment', fs_code)
+replace_shader_in_actor(canvas_actor, "fragment", fs_code)
 
 scene.add(canvas_actor)
 
