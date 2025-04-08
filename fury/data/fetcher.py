eb04e4ab2eda4132b35998bd09c7e116bd652f49
Javier Guaje <jrguajeg@iu.edu>
2025-04-08 16:24:09 -0400

    Restored fury/data/fetcher.py.

--- fury/data/fetcher.py
+++ fury/data/fetcher.py
@@ -333,7 +333,6 @@ async def _download(session, url, filename, *, size=None):
         Name of the downloaded file (e.g. BoxTextured.gltf)
     size : int, optional
         Length of the content in bytes
-    
     """
     if not os.path.exists(filename):
         print(f"Downloading: {filename}")
