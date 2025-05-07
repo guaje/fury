9346f9c3c51048862267bf933e47a8dacdfc4188
Javier Guaje <jrguajeg@iu.edu>
2025-05-07 08:44:07 -0400

    Minor changes to README and ray_traced_1.0.py.

--- docs/experimental/sh_odf/ray_tracing/ray_traced_1.0.py
+++ docs/experimental/sh_odf/ray_tracing/ray_traced_1.0.py
@@ -98,51 +98,55 @@ if __name__ == "__main__":
     """
 
     eval_sh_2 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_2.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_2.frag")
     )
 
     eval_sh_4 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_4.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_4.frag")
     )
 
     eval_sh_6 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_6.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_6.frag")
     )
 
     eval_sh_8 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_8.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_8.frag")
     )
 
     eval_sh_10 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_10.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_10.frag")
     )
 
     eval_sh_12 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_12.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_12.frag")
     )
 
     eval_sh_grad_2 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_2.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_grad_2.frag")
     )
 
     eval_sh_grad_4 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_4.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_grad_4.frag")
     )
 
     eval_sh_grad_6 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_6.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_grad_6.frag")
     )
 
     eval_sh_grad_8 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_8.frag")
+        os.path.join("ray_tracing", "odf", "descoteaux", "eval_sh_grad_8.frag")
     )
 
     eval_sh_grad_10 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_10.frag")
+        os.path.join(
+            "ray_tracing", "odf", "descoteaux", "eval_sh_grad_10.frag"
+        )
     )
 
     eval_sh_grad_12 = import_fury_shader(
-        os.path.join("rt_odfs", "descoteaux", "eval_sh_grad_12.frag")
+        os.path.join(
+            "ray_tracing", "odf", "descoteaux", "eval_sh_grad_12.frag"
+        )
     )
 
     # Searches a single root of a polynomial within a given interval.
@@ -162,13 +166,13 @@ if __name__ == "__main__":
     #
     #   return true if a root was found, false if no root exists.
     newton_bisection = import_fury_shader(
-        os.path.join("rt_odfs", "newton_bisection.frag")
+        os.path.join("utils", "newton_bisection.frag")
     )
 
     # Finds all roots of the given polynomial in the interval [begin, end] and
     # writes them to out_roots. Some entries will be NO_INTERSECTION but other
     # than that the array is sorted. The last entry is always NO_INTERSECTION.
-    find_roots = import_fury_shader(os.path.join("rt_odfs", "find_roots.frag"))
+    find_roots = import_fury_shader(os.path.join("utils", "find_roots.frag"))
 
     # Evaluates the spherical harmonics basis in bands 0, 2, ..., SH_DEGREE.
     # Conventions are as in the following paper.
@@ -179,18 +183,20 @@ if __name__ == "__main__":
     #       SH_DEGREE in this order.
     #   param point The point on the unit sphere where the basis should be
     #       evaluated.
-    eval_sh = import_fury_shader(os.path.join("rt_odfs", "eval_sh.frag"))
+    eval_sh = import_fury_shader(
+        os.path.join("ray_tracing", "odf", "eval_sh.frag")
+    )
 
     # Evaluates the gradient of each basis function given by eval_sh() and the
     # basis itself
     eval_sh_grad = import_fury_shader(
-        os.path.join("rt_odfs", "eval_sh_grad.frag")
+        os.path.join("ray_tracing", "odf", "eval_sh_grad.frag")
     )
 
     # Outputs a matrix that turns equidistant samples on the unit circle of a
     # homogeneous polynomial into coefficients of that polynomial.
     get_inv_vandermonde = import_fury_shader(
-        os.path.join("rt_odfs", "get_inv_vandermonde.frag")
+        os.path.join("ray_tracing", "odf", "get_inv_vandermonde.frag")
     )
 
     # Determines all intersections between a ray and a spherical harmonics
@@ -204,7 +210,7 @@ if __name__ == "__main__":
     #   param ray_origin The origin of the ray, relative to the glyph center.
     #   param ray_dir The normalized direction vector of the ray.
     ray_sh_glyph_intersections = import_fury_shader(
-        os.path.join("rt_odfs", "ray_sh_glyph_intersections.frag")
+        os.path.join("ray_tracing", "odf", "ray_sh_glyph_intersections.frag")
     )
 
     # Provides a normalized normal vector for a spherical harmonics glyph.
@@ -215,7 +221,7 @@ if __name__ == "__main__":
     #
     #   return A normalized surface normal pointing away from the origin.
     get_sh_glyph_normal = import_fury_shader(
-        os.path.join("rt_odfs", "get_sh_glyph_normal.frag")
+        os.path.join("ray_tracing", "odf", "get_sh_glyph_normal.frag")
     )
 
     # This is the glTF BRDF for dielectric materials, exactly as described
@@ -228,32 +234,72 @@ if __name__ == "__main__":
     #   param base_color The albedo used for the Lambertian diffuse component.
     #
     #   return The BRDF for the given directions.
-    gltf_dielectric_brdf = import_fury_shader(
-        os.path.join("rt_odfs", "gltf_dielectric_brdf.frag")
-    )
+    gltf_dielectric_brdf = """
+    vec3 gltf_dielectric_brdf(vec3 incoming, vec3 outgoing, vec3 normal, float roughness, vec3 base_color)
+    {
+        float ni = dot(normal, incoming);
+        float no = dot(normal, outgoing);
+        // Early out if incoming or outgoing direction are below the horizon
+        if (ni <= 0.0 || no <= 0.0)
+            return vec3(0.0);
+        // Save some work by not actually computing the half-vector. If the half-
+        // vector were h, ih = dot(incoming, h) and
+        // sqrt(nh_ih_2 / ih_2) = dot(normal, h).
+        float ih_2 = dot(incoming, outgoing) * 0.5 + 0.5;
+        float sum = ni + no;
+        float nh_ih_2 = 0.25 * sum * sum;
+        float ih = sqrt(ih_2);
+
+        // Evaluate the GGX normal distribution function
+        float roughness_2 = roughness * roughness;
+        float roughness_4  = roughness_2 * roughness_2;
+        float roughness_flip = 1.0 - roughness_4;
+        float denominator = ih_2 - nh_ih_2 * roughness_flip;
+        float ggx = (roughness_4 * M_INV_PI * ih_2) / (denominator * denominator);
+        // Evaluate the "visibility" (i.e. masking-shadowing times geometry terms)
+        float vi = ni + sqrt(roughness_4 + roughness_flip * ni * ni);
+        float vo = no + sqrt(roughness_4 + roughness_flip * no * no);
+        float v = 1.0 / (vi * vo);
+        // That completes the specular BRDF
+        float specular = v * ggx;
+
+        // The diffuse BRDF is Lambertian
+        vec3 diffuse = M_INV_PI * base_color;
+
+        // Evaluate the Fresnel term using the Fresnel-Schlick approximation
+        const float ior = 1.5;
+        const float f0 = ((1.0 - ior) / (1.0 + ior)) * ((1.0 - ior) / (1.0 + ior));
+        float ih_flip = 1.0 - ih;
+        float ih_flip_2 = ih_flip * ih_flip;
+        float fresnel = f0 + (1.0 - f0) * ih_flip * ih_flip_2 * ih_flip_2;
+
+        // Mix the two components
+        return mix(diffuse, vec3(specular), fresnel);
+    }
+    """
 
     # Applies the non-linearity that maps linear RGB to sRGB
     linear_to_srgb = import_fury_shader(
-        os.path.join("rt_odfs", "linear_to_srgb.frag")
+        os.path.join("lighting", "linear_to_srgb.frag")
     )
 
     # Inverse of linear_to_srgb()
     srgb_to_linear = import_fury_shader(
-        os.path.join("rt_odfs", "srgb_to_linear.frag")
+        os.path.join("lighting", "srgb_to_linear.frag")
     )
 
     # Turns a linear RGB color (i.e. rec. 709) into sRGB
     linear_rgb_to_srgb = import_fury_shader(
-        os.path.join("rt_odfs", "linear_rgb_to_srgb.frag")
+        os.path.join("lighting", "linear_rgb_to_srgb.frag")
     )
 
     # Inverse of linear_rgb_to_srgb()
     srgb_to_linear_rgb = import_fury_shader(
-        os.path.join("rt_odfs", "srgb_to_linear_rgb.frag")
+        os.path.join("lighting", "srgb_to_linear_rgb.frag")
     )
 
     # Logarithmic tonemapping operator. Input and output are linear RGB.
-    tonemap = import_fury_shader(os.path.join("rt_odfs", "tonemap.frag"))
+    tonemap = import_fury_shader(os.path.join("lighting", "tonemap.frag"))
 
     # fmt: off
     fs_dec = compose_shader([
@@ -303,72 +349,75 @@ if __name__ == "__main__":
 
     # Define SH coefficients (measured up to band 8, noise beyond that)
     sh_coeffs = """
-    float sh_coeffs[SH_COUNT];
-    sh_coeffs[0] = -0.2739740312099; sh_coeffs[1] = 0.2526670396328;
-    sh_coeffs[2] = 1.8922271728516; sh_coeffs[3] = 0.2878578901291;
-    sh_coeffs[4] = -0.5339795947075; sh_coeffs[5] = -0.2620058953762;
+    float shCoeffs[SH_COUNT];
+    shCoeffs[0] = -0.2739740312099; shCoeffs[1] = 0.2526670396328;
+    shCoeffs[2] = 1.8922271728516; shCoeffs[3] = 0.2878578901291;
+    shCoeffs[4] = -0.5339795947075; shCoeffs[5] = -0.2620058953762;
     #if SH_DEGREE >= 4
-        sh_coeffs[6] = 0.1580424904823; sh_coeffs[7] = 0.0329004973173;
-        sh_coeffs[8] = -0.1322413831949; sh_coeffs[9] = -0.1332057565451;
-        sh_coeffs[10] = 1.0894461870193; sh_coeffs[11] = -0.6319401264191;
-        sh_coeffs[12] = -0.0416776277125; sh_coeffs[13] = -1.0772529840469;
-        sh_coeffs[14] = 0.1423762738705;
+        shCoeffs[6] = 0.1580424904823; shCoeffs[7] = 0.0329004973173;
+        shCoeffs[8] = -0.1322413831949; shCoeffs[9] = -0.1332057565451;
+        shCoeffs[10] = 1.0894461870193; shCoeffs[11] = -0.6319401264191;
+        shCoeffs[12] = -0.0416776277125; shCoeffs[13] = -1.0772529840469;
+        shCoeffs[14] = 0.1423762738705;
     #endif
     #if SH_DEGREE >= 6
-        sh_coeffs[15] = 0.7941166162491; sh_coeffs[16] = 0.7490307092667;
-        sh_coeffs[17] = -0.3428381681442; sh_coeffs[18] = 0.1024847552180;
-        sh_coeffs[19] = -0.0219132602215; sh_coeffs[20] = 0.0499043911695;
-        sh_coeffs[21] = 0.2162453681231; sh_coeffs[22] = 0.0921059995890;
-        sh_coeffs[23] = -0.2611238956451; sh_coeffs[24] = 0.2549301385880;
-        sh_coeffs[25] = -0.4534865319729; sh_coeffs[26] = 0.1922748684883;
-        sh_coeffs[27] = -0.6200597286224;
+        shCoeffs[15] = 0.7941166162491; shCoeffs[16] = 0.7490307092667;
+        shCoeffs[17] = -0.3428381681442; shCoeffs[18] = 0.1024847552180;
+        shCoeffs[19] = -0.0219132602215; shCoeffs[20] = 0.0499043911695;
+        shCoeffs[21] = 0.2162453681231; shCoeffs[22] = 0.0921059995890;
+        shCoeffs[23] = -0.2611238956451; shCoeffs[24] = 0.2549301385880;
+        shCoeffs[25] = -0.4534865319729; shCoeffs[26] = 0.1922748684883;
+        shCoeffs[27] = -0.6200597286224;
     #endif
     #if SH_DEGREE >= 8
-        sh_coeffs[28] = -0.0532187558711; sh_coeffs[29] = -0.3569841980934;
-        sh_coeffs[30] = 0.0293972902000; sh_coeffs[31] = -0.1977960765362;
-        sh_coeffs[32] = -0.1058669015765; sh_coeffs[33] = 0.2372217923403;
-        sh_coeffs[34] = -0.1856198310852; sh_coeffs[35] = -0.3373193442822;
-        sh_coeffs[36] = -0.0750469490886; sh_coeffs[37] = 0.2146576642990;
-        sh_coeffs[38] = -0.0490148440003; sh_coeffs[39] = 0.1288588196039;
-        sh_coeffs[40] = 0.3173974752426; sh_coeffs[41] = 0.1990085393190;
-        sh_coeffs[42] = -0.1736343950033; sh_coeffs[43] = -0.0482443645597;
-        sh_coeffs[44] = 0.1749017387629;
+        shCoeffs[28] = -0.0532187558711; shCoeffs[29] = -0.3569841980934;
+        shCoeffs[30] = 0.0293972902000; shCoeffs[31] = -0.1977960765362;
+        shCoeffs[32] = -0.1058669015765; shCoeffs[33] = 0.2372217923403;
+        shCoeffs[34] = -0.1856198310852; shCoeffs[35] = -0.3373193442822;
+        shCoeffs[36] = -0.0750469490886; shCoeffs[37] = 0.2146576642990;
+        shCoeffs[38] = -0.0490148440003; shCoeffs[39] = 0.1288588196039;
+        shCoeffs[40] = 0.3173974752426; shCoeffs[41] = 0.1990085393190;
+        shCoeffs[42] = -0.1736343950033; shCoeffs[43] = -0.0482443645597;
+        shCoeffs[44] = 0.1749017387629;
     #endif
     #if SH_DEGREE >= 10
-        sh_coeffs[45] = -0.0151847425660; sh_coeffs[46] = 0.0418366046081;
-        sh_coeffs[47] = 0.0863263587216; sh_coeffs[48] = -0.0649211244490;
-        sh_coeffs[49] = 0.0126096132283; sh_coeffs[50] = 0.0545089217982;
-        sh_coeffs[51] = -0.0275142164626; sh_coeffs[52] = 0.0399986574832;
-        sh_coeffs[53] = -0.0468244261610; sh_coeffs[54] = -0.1292105653111;
-        sh_coeffs[55] = -0.0786858322658; sh_coeffs[56] = -0.0663828464882;
-        sh_coeffs[57] = 0.0382439706831; sh_coeffs[58] = -0.0041550330365;
-        sh_coeffs[59] = -0.0502800566338; sh_coeffs[60] = -0.0732471630735;
-        sh_coeffs[61] = 0.0181751900972; sh_coeffs[62] = -0.0090119333757;
-        sh_coeffs[63] = -0.0604443282359; sh_coeffs[64] = -0.1469985252752;
-        sh_coeffs[65] = -0.0534046899715;
+        shCoeffs[45] = -0.0151847425660; shCoeffs[46] = 0.0418366046081;
+        shCoeffs[47] = 0.0863263587216; shCoeffs[48] = -0.0649211244490;
+        shCoeffs[49] = 0.0126096132283; shCoeffs[50] = 0.0545089217982;
+        shCoeffs[51] = -0.0275142164626; shCoeffs[52] = 0.0399986574832;
+        shCoeffs[53] = -0.0468244261610; shCoeffs[54] = -0.1292105653111;
+        shCoeffs[55] = -0.0786858322658; shCoeffs[56] = -0.0663828464882;
+        shCoeffs[57] = 0.0382439706831; shCoeffs[58] = -0.0041550330365;
+        shCoeffs[59] = -0.0502800566338; shCoeffs[60] = -0.0732471630735;
+        shCoeffs[61] = 0.0181751900972; shCoeffs[62] = -0.0090119333757;
+        shCoeffs[63] = -0.0604443282359; shCoeffs[64] = -0.1469985252752;
+        shCoeffs[65] = -0.0534046899715;
     #endif
     #if SH_DEGREE >= 12
-        sh_coeffs[66] = -0.0896672753415; sh_coeffs[67] = -0.0130841364808;
-        sh_coeffs[68] = -0.0112942893801; sh_coeffs[69] = 0.0272257498541;
-        sh_coeffs[70] = 0.0626717616331; sh_coeffs[71] = -0.0222197983050;
-        sh_coeffs[72] = -0.0018541504308; sh_coeffs[73] = -0.1653251944056;
-        sh_coeffs[74] = 0.0409697402846; sh_coeffs[75] = 0.0749921454327;
-        sh_coeffs[76] = -0.0282830872616; sh_coeffs[77] = 0.0006909458525;
-        sh_coeffs[78] = 0.0625599842287; sh_coeffs[79] = 0.0812529816082;
-        sh_coeffs[80] = 0.0914693020772; sh_coeffs[81] = -0.1197222726745;
-        sh_coeffs[82] = 0.0376277453183; sh_coeffs[83] = -0.0832617004142;
-        sh_coeffs[84] = -0.0482175038043; sh_coeffs[85] = -0.0839003635737;
-        sh_coeffs[86] = -0.0349423908400; sh_coeffs[87] = 0.1204519568256;
-        sh_coeffs[88] = 0.0783745984003; sh_coeffs[89] = 0.0297401205976;
-        sh_coeffs[90] = -0.0505947662525;
+        shCoeffs[66] = -0.0896672753415; shCoeffs[67] = -0.0130841364808;
+        shCoeffs[68] = -0.0112942893801; shCoeffs[69] = 0.0272257498541;
+        shCoeffs[70] = 0.0626717616331; shCoeffs[71] = -0.0222197983050;
+        shCoeffs[72] = -0.0018541504308; shCoeffs[73] = -0.1653251944056;
+        shCoeffs[74] = 0.0409697402846; shCoeffs[75] = 0.0749921454327;
+        shCoeffs[76] = -0.0282830872616; shCoeffs[77] = 0.0006909458525;
+        shCoeffs[78] = 0.0625599842287; shCoeffs[79] = 0.0812529816082;
+        shCoeffs[80] = 0.0914693020772; shCoeffs[81] = -0.1197222726745;
+        shCoeffs[82] = 0.0376277453183; shCoeffs[83] = -0.0832617004142;
+        shCoeffs[84] = -0.0482175038043; shCoeffs[85] = -0.0839003635737;
+        shCoeffs[86] = -0.0349423908400; shCoeffs[87] = 0.1204519568256;
+        shCoeffs[88] = 0.0783745984003; shCoeffs[89] = 0.0297401205976;
+        shCoeffs[90] = -0.0505947662525;
     #endif
     """
 
     # Perform the intersection test
     intersection_test = """
-    float ray_params[MAX_DEGREE];
-    //ray_sh_glyph_intersections(ray_params, sh_coeffs, camera_pos, ray_dir);
-    ray_sh_glyph_intersections(ray_params, sh_coeffs, ro, rd);
+    float rayParams[MAX_DEGREE];
+    rayGlyphIntersections(
+        rayParams, shCoeffs, ro - centerMCVSOutput, rd, int(shDegree),
+        int(numCoeffsVSOutput), int(maxPolyDegreeVSOutput), M_PI,
+        NO_INTERSECTION
+    );
     """
 
     # Identify the first intersection
@@ -389,7 +438,7 @@ if __name__ == "__main__":
     if (first_ray_param != NO_INTERSECTION) {
         //vec3 intersection = camera_pos + first_ray_param * ray_dir;
         vec3 intersection = ro + first_ray_param * rd;
-        vec3 normal = get_sh_glyph_normal(sh_coeffs, intersection);
+        vec3 normal = getShGlyphNormal(sh_coeffs, intersection);
         vec3 base_color = srgb_to_linear_rgb(abs(normalize(intersection)));
         const vec3 incoming = normalize(vec3(1.23, -4.56, 7.89));
         float ambient = 0.04;
